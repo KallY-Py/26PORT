3e428d450041b9d66713dd9f6c6471259bd917a1
KallY-Py <jcporcopio03@gmail.com>
2026-06-23 13:29:20 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production' 
     ? '/26PORT/' 
     : '/',
-  outputDir: 'docs', 
+  outputDir: 'docs',
   assetsDir: 'assets',
   indexPath: 'index.html',
   filenameHashing: true,
